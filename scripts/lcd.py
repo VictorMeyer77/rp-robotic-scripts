07a6bc1fb2420a1d9bcb592df3512a11ad80812c
Victor-Algo <victor.algo.contact@gmail.com>
2023-04-21 00:34:04 +0200

    lcd test

--- scripts/lcd.py
+++ scripts/lcd.py
@@ -1,6 +1,9 @@
 from RPLCD.gpio import CharLCD
 from RPi import GPIO
+import time
 
 lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23], numbering_mode=GPIO.BOARD)
 
-lcd.write_string(u'Espace Raspberry    Francais')
+lcd.write_string("TAIVE!!!!")
+time.sleep(5)
+lcd.clear()

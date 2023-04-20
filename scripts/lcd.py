772d31102a95d11c7d6977e1b538c14bd498e18a
Victor-Algo <victor.algo.contact@gmail.com>
2023-04-21 00:43:39 +0200

    lcd test

--- scripts/lcd.py
+++ scripts/lcd.py
@@ -5,7 +5,7 @@ import time
 lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23],
               numbering_mode=GPIO.BOARD, auto_linebreaks=True)
 
-lcd.write_string("TAIVE!!!!")
+lcd.write_string("HELLO MEYER !!!!")
 time.sleep(5)
 lcd.clear()
 

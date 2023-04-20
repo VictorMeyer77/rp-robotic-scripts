dfffbe4d9581c8d24fdbc8e17d13656da3a60a1c
Victor-Algo <victor.algo.contact@gmail.com>
2023-04-20 22:56:59 +0200

    lcd test

--- scripts/lcd.py
+++ scripts/lcd.py
@@ -1,8 +1,6 @@
 from RPLCD.gpio import CharLCD
-import RPi.GPIO as GPIO
+from RPi import GPIO
 
-GPIO.setmode(GPIO.BOARD)
-
-lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23])
+lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23], numbering_mode=GPIO.BOARD)
 
 lcd.write_string(u'Espace Raspberry    Francais')

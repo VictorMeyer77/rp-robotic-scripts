ec41e7aa35782c4ed0b25e589998787977aeb954
Victor-Algo <victor.algo.contact@gmail.com>
2023-04-21 20:31:36 +0200

    lcd test

--- scripts/lcd.py
+++ scripts/lcd.py
@@ -3,15 +3,23 @@ from RPi import GPIO
 import time
 
 
-lcd = CharLCD(cols=16,
-              rows=2,
-              pin_rs=37,
-              pin_e=35,
-              pins_data=[33, 31, 29, 23],
-              numbering_mode=GPIO.BOARD,
-              auto_linebreaks=True)
+def display(char_lcd):
+    char_lcd.write_string("HELLO,\nWORLD !!!!")
+    time.sleep(5)
 
-lcd.write_string("HELLO,\n\rWORLD !!!! 😀".encode("utf-8"))
-time.sleep(5)
-lcd.clear()
-GPIO.cleanup()
+
+def shutdown(char_lcd):
+    char_lcd.clear()
+    GPIO.cleanup()
+
+
+if __name__ == "__main__":
+    lcd = CharLCD(cols=16,
+                  rows=2,
+                  pin_rs=37,
+                  pin_e=35,
+                  pins_data=[33, 31, 29, 23],
+                  numbering_mode=GPIO.BOARD,
+                  auto_linebreaks=True)
+    display(lcd)
+    shutdown(lcd)

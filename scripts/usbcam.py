3e33cc97996b63723d37014ac3d176d274d69a85
Victor-Algo <victor.algo.contact@gmail.com>
2023-04-25 21:24:32 +0200

    minor changes

--- scripts/usbcam.py
+++ scripts/usbcam.py
@@ -1,11 +1,17 @@
+"""
+
+A tester
+
+"""
+
 import cv2
 
 vid = cv2.VideoCapture(0)
 
 while True:
     ret, frame = vid.read()
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow("frame", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         cv2.imwrite("resources/output/image/webcam/image_buffer.png", frame)
         break
 

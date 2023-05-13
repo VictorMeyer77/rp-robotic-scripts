a40cf23d7a0f2a29f39d1c7223d1e83fcaf2153f
Victor-Algo <victor.algo.contact@gmail.com>
2023-05-13 23:55:54 +0200

    rollback to cv2

--- scripts/camera.py
+++ scripts/camera.py
@@ -1,30 +1,15 @@
-"""
-    sudo apt-get install python3-picamera
-"""
-
-import time
-import picamera
-import datetime
+import cv2
 import os
 
 OUTPUT_DIR = "resources/output/image/camera"
 
 
-def image(camera, output_dir):
-    camera.capture(os.path.join(output_dir, str(datetime.datetime.now()) + '.jpg'))
-
-
-def video(camera, output_dir):
-    name = os.path.join(output_dir, time.strftime("%d.%m.%Y-%Hh%Mm%Ss"))
-    camera.start_recording(name + ".h264")
-    time.sleep(10)
-    camera.stop_recording()
-    command = "MP4Box -add " + name + ".h264 " + name + ".mp4"
-    os.system(command)
+def image(cam, output_dir):
+    ret, frame = cam.read()
+    cv2.imwrite(os.path.join(output_dir, "image_buffer.png"), frame)
 
 
 if __name__ == "__main__":
-    cam = picamera.PiCamera()
-    cam.resolution = (2592, 1944)
-    image(cam, OUTPUT_DIR)
-    video(cam, OUTPUT_DIR)
+    camera = cv2.VideoCapture(0)
+    image(camera, OUTPUT_DIR)
+    camera.release()

27545acc1a6b1d262e8db815e19be768c45fcd2e
Victor-Algo <victor.algo.contact@gmail.com>
2023-05-28 23:06:54 +0200

    test picamera

--- scripts/camera.py
+++ scripts/camera.py
@@ -1,9 +1,11 @@
-import cv2
-import os
+from time import sleep
+
+#import cv2
+#import os
 
 OUTPUT_DIR = "resources/output/image/camera"
 
-
+"""
 def image(cam, output_dir):
     ret, frame = cam.read()
     cv2.imwrite(os.path.join(output_dir, "image_buffer.png"), frame)
@@ -13,3 +15,11 @@ if __name__ == "__main__":
     camera = cv2.VideoCapture(0)
     image(camera, OUTPUT_DIR)
     camera.release()
+
+"""
+camera = PiCamera()
+camera.resolution = (64, 64)
+camera.start_preview()
+# Camera warm-up time
+sleep(2)
+camera.capture("resources/output/image/camera/test.jpg")
